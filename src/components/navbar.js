125aff847df37b8f59cf2040ebd4e674778bdf93
Ariel <Ariel.Salem1989@gmail.com>
2017-07-20 08:46:01 -0700

    navbar functional - hurray!

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,41 @@
  import React, { Component } from 'react';
- import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
  import { Link } from 'react-router-dom';
+ import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
 
  export default class Navigation extends Component {
    constructor(props) {
       super(props);
         this.toggle = this.toggle.bind(this);
         this.state = {
-          isOpen: false
+          dropdownOpen: false
         };
     }
 
     toggle() {
-      console.log('this is toggled')
       this.setState({
-        isOpen: !this.state.isOpen
+        dropdownOpen: !this.state.dropdownOpen
       });
     }
 
    render() {
      return (
       <nav className="navbar navbar-toggleable-md navbar-light bg-faded navMaster">
-        <button className="navbar-toggler navbar-toggler-right btn" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">MENU  <span className="navbar-toggler-icon" onClick={this.toggle}></span>
-        </button>
+        <Dropdown className="navbar-toggler navbar-toggler-right btn" type="button" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle>
+              <span className="menu-button">MENU <span className="navbar-toggler-icon" onClick={this.toggle}>
+              </span></span>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem><li className="nav-item">
+                <a className="nav-link" href="/#portfolio">PORTFOLIO</a>
+              </li></DropdownItem>
+            <DropdownItem><Link className="nav-link" to="/blog">BLOG</Link></DropdownItem>
+            <DropdownItem><li className="nav-item">
+                <a className="nav-link" href="/#contact">CONTACT</a>
+              </li></DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <Link className="navbar-brand" to="/"><strong>AS ARIEL SALEM</strong></Link>
         <div  id="navbarNavDropdown" className="navbar-collapse collapse">
           <ul className="navbar-nav mr-auto">
