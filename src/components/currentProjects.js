862bbfe4dfa40f91eefd3eb6e94e0ba40ac3fae9
Ariel <Ariel.Salem1989@gmail.com>
2017-07-20 15:50:53 -0700

    restyled portfolio

--- src/components/currentProjects.js
+++ src/components/currentProjects.js
@@ -6,7 +6,7 @@ export default class CurrentProjects extends Component {
     return (
      <div id="portfolio" className="project-info">
       <div className="row">
-        <div className="col-md-3 offset-md-1 project-body-left">
+        <div className="col-md-3 offset-md-1 col-sm-11 project-body">
           <div className="row">
             <a href="https://organico-plantr.herokuapp.com">
              <img className="project-picture" src="../../public/img/Plantr.png" />
@@ -26,9 +26,9 @@ export default class CurrentProjects extends Component {
            </div>
          </div>
         </div>
-        <div className="col-md-3 project-body-right">
+        <div className="col-md-3 col-sm-11 project-body">
           <div className="row"><a href="https://stackets-orion.herokuapp.com">
-             <img className="project-picture-right" src="../../public/img/Plantr.png" />
+             <img className="project-picture" src="../../public/img/Plantr.png" />
            </a>
          </div>
           <div className="row">
@@ -36,7 +36,7 @@ export default class CurrentProjects extends Component {
            <div className="project-info">
            <p className="project-name">STACKETS</p>
            <div className="project-description">
-             <p>Plantr is an online gardening and outdoor decor service where you can create and plan your seasonal gardens based on regional variations and personal needs.</p>
+             <p>Stackets is a personalized programming assistant that allows users to work through, solve, and store helpful tips and tricks for computer programming.</p>
              <div className="project-role">
                <b>ROLE: </b>
                <i>Product Owner</i>
@@ -45,7 +45,7 @@ export default class CurrentProjects extends Component {
           </div>
          </div>
         </div>
-        <div className="col-md-3 project-body-left">
+        <div className="col-md-3 col-sm-11 project-body">
             <div className="row"><a href="https://organico-plantr.herokuapp.com">
                <img className="project-picture" src="../../public/img/Plantr.png" />
              </a></div>
@@ -54,7 +54,7 @@ export default class CurrentProjects extends Component {
              <div className="project-info">
              <p className="project-name">THYMER</p>
              <div className="project-description">
-               <p>Plantr is an online gardening and outdoor decor service where you can create and plan your seasonal gardens based on regional variations and personal needs. </p>
+               <p>Thymer is the personal Sous Chef you never knew you needed. From automated timers that keep you on track, to vocalized recipes, this app makes cooking fun and easy. </p>
                <div className="project-role">
                  <b>ROLE: </b>
                  <i>Software Engineer</i>
