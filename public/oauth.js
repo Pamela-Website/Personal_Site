d0748392e65ca49ca418aca02b0f106ea351c56c
Ariel <Ariel.Salem1989@gmail.com>
2017-07-22 17:24:54 -0700

    pushed to heroku

--- public/oauth.js
+++
@@ -1,6 +0,0 @@
-module.exports.user = 'Ariel.Salem1989@gmail.com';
-module.exports.pass = 'As58254651973';
-module.exports.clientId = '720304526748-1hohbsi7kuhr0rra6p07tj936e7p39sv.apps.googleusercontent.com';
-module.exports.secret = '8uin_ziq_itK09ut2j0ozEhO';
-module.exports.token = '1/JLmwFCzkILkKjtrlRQhZaUDV1xEbasDrinawL2IL8Qs';
-module.exports.access = 'ya29.GluOBEuJP03QQIjHji7z37GS6jzpmqhADZ8w8RvAIWK6iy4oWtPHs7hYDMfEZ24s7RJ1WXsE-DTCH9poUdf5-g--V15JXF-cnNyRxwC4QB6YPq2z7khTAsCD4F6p';
