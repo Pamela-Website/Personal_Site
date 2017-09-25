651f1f6918a96c91111443a4041adcc6e576fb93
Ariel <Ariel.Salem1989@gmail.com>
2017-09-25 08:49:04 +0300

    adjusted landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,28 +10,32 @@ export default class Landing extends Component {
   intro() {
     return (
       <div className="intro">
-        <div className='row intro-name'>
-          <img className="intro-logo" src="../../public/img/concept-white.png" />
-        </div>
-        <div className='row intro-description'>
-          <p className="intro-description">Whole Foods Nutrition | Recipe Creation | Mind-Body Wellness | Women’s Health & Wellbeing </p>
-        </div>
-        <div className='row enter-button'>
-          <button className="enter" onClick={this.enterSite}>ENTER</button>
-        </div>
-        <div className="intro-icons">
-          <a target="_blank" className="a-intro" href="contact">
-            <i className="fa fa-envelope" aria-hidden="true"/>
-          </a>
-          <a target="_blank" className="a-intro" href="https://www.facebook.com/nourish.roots.1">
-            <i className="fa fa-facebook-official" aria-hidden="true"/>
-          </a>
-          <a target="_blank" className="a-intro" href="https://www.instagram.com/nourishroots/">
-            <i className="fa fa-instagram" aria-hidden="true"/>
-          </a>
-          <a target="_blank" className="a-intro" href="https://www.pinterest.com/nourishroots/">
-            <i className="fa fa-pinterest" aria-hidden="true"/>
-          </a>
+        <div className="row">
+          <div className="col-xs-12">
+          <div className='row intro-name'>
+            <img className="intro-logo" src="../../public/img/concept-white.png" />
+          </div>
+          <div className='row intro-description'>
+            <p className="intro-description">Whole Foods Nutrition | Recipe Creation | Mind-Body Wellness | Women’s Health & Wellbeing </p>
+          </div>
+          <div className='row enter-button'>
+            <button className="enter" onClick={this.enterSite}>ENTER</button>
+          </div>
+          <div className="intro-icons">
+            <a target="_blank" className="a-intro" href="contact">
+              <i className="fa fa-envelope" aria-hidden="true"/>
+            </a>
+            <a target="_blank" className="a-intro" href="https://www.facebook.com/nourish.roots.1">
+              <i className="fa fa-facebook-official" aria-hidden="true"/>
+            </a>
+            <a target="_blank" className="a-intro" href="https://www.instagram.com/nourishroots/">
+              <i className="fa fa-instagram" aria-hidden="true"/>
+            </a>
+            <a target="_blank" className="a-intro" href="https://www.pinterest.com/nourishroots/">
+              <i className="fa fa-pinterest" aria-hidden="true"/>
+            </a>
+          </div>
+          </div>
         </div>
       </div>
     )
