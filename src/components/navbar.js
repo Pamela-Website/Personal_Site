5c0e8e91fce9a45424355cf89a7ae0c766dcf51d
Ariel <Ariel.Salem1989@gmail.com>
2017-09-30 08:26:04 +0300

    undid styling for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,75 +4,95 @@
  import { Menu, IconMenu, MenuItem } from 'react-toolbox/lib/menu';
 
  export default class Navigation extends Component {
-   constructor(props) {
-      super(props);
-        this.state = {
-          dropdownOpen: false,
-        };
-        this.toggle = this.toggle.bind(this);
-    }
-
-    toggle() {
-      this.setState({
-        dropdownOpen: !this.state.dropdownOpen
-      });
-    }
-
-   render() {
-     return (
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded navMaster">
-        <Dropdown className="navbar-toggler navbar-toggler-right btn" type="button" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle>
-              <span className="menu-button">MENU <span className="navbar-toggler-icon" onClick={this.toggle}>
-              </span></span>
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem><Link className="nav-link" to="/about">ABOUT</Link></DropdownItem>
-            <DropdownItem><Link className="nav-link" to="/offerings">OFFERINGS</Link></DropdownItem>
-            <DropdownItem><Link className="nav-link" to="/resources">RESOURCES</Link></DropdownItem>
-            <DropdownItem><Link className="nav-link" to="/blog">BLOG</Link></DropdownItem>
-            <DropdownItem><Link className="nav-link" to='/contact'>CONTACT</Link></DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <Link className="navbar-brand" to="/resources">
-          <img className="logo" src="../../public/img/concept4-white.png" />
-        </Link>
-        <div  id="navbarNavDropdown" className="navbar-collapse collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">ABOUT</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/resources">RESOURCES</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/blog">BLOG</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">CONTACT</Link>
-            </li>
-            <li className="nav-item nav-menu-item">
-              <IconMenu
-                icon='OFFERINGS'
-                menuRipple
-              >
-                <MenuItem>
-                    <Link className="menu-item" to="/nutritional-counseling">NUTRITIONAL COUNSELING</Link>
-                </MenuItem>
-                <MenuItem >
-                    <Link className="menu-item" to="/group-sessions">GROUP SESSIONS</Link>
-                </MenuItem>
-                <MenuItem>
-                    <Link className="menu-item" to="/corporate-wellness">CORPORATE WELLNESS</Link>
-                </MenuItem>
-              </IconMenu>
-            </li>
-          </ul>
-        </div>
-    </nav>
-    )
+  constructor(props) {
+     super(props);
+       this.state = {
+         dropdownOpen: false,
+       };
+       this.toggle = this.toggle.bind(this);
    }
+
+   toggle() {
+     this.setState({
+       dropdownOpen: !this.state.dropdownOpen
+     });
+   }
+
+  render() {
+    return (
+     <nav className="navbar navbar-toggleable-md navbar-light bg-faded navMaster">
+       <Dropdown className="navbar-toggler navbar-toggler-right btn" type="button" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+         <DropdownToggle>
+             <span className="menu-button">MENU<span className="navbar-toggler-icon" onClick={this.toggle}>
+             </span></span>
+         </DropdownToggle>
+         <DropdownMenu>
+           <DropdownItem><Link className="nav-link" to="/about-me">ABOUT ME</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/health-philosophy">HEALTH PHILOSOPHY</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/testimonials">TESTIMONIALS</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/resources">RESOURCES</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/recipes">RECIPES</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/blog">BLOG</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to='/contact'>CONTACT</Link></DropdownItem>
+           <DropdownItem><Link className="nav-link" to="/services">SERVICES</Link></DropdownItem>
+         </DropdownMenu>
+       </Dropdown>
+       <Link className="navbar-brand" to="/resources">
+         <img className="logo" src="../../public/img/concept4-white.png" />
+       </Link>
+       <div  id="navbarNavDropdown" className="navbar-collapse collapse">
+         <ul className="navbar-nav mr-auto">
+           <li className="nav-item">
+             <Link className="nav-link" to="/resources">RESOURCES</Link>
+           </li>
+           <li className="nav-item">
+             <Link className="nav-link" to="/recipes">RECIPES</Link>
+           </li>
+           <li className="nav-item">
+             <Link className="nav-link" to="/blog">BLOG</Link>
+           </li>
+           <li className="nav-item">
+             <IconMenu
+               icon='SERVICES'
+               iconRipple={false}
+             >
+               <Link className='menu-item' to='/nutritional-counseling' >
+                 <MenuItem className='menu-item' caption='NUTRITIONAL COUNSELING' />
+               </Link>
+               <Link className="menu-item" to="/group-sessions">
+                 <MenuItem className='menu-item' caption='GROUP SESSIONS' />
+               </Link>
+               <Link className="menu-item" to="/corporate-wellness">
+                 <MenuItem className='menu-item' caption='CORPORATE WELLNESS' />
+               </Link>
+             </IconMenu>
+           </li>
+           <li className="nav-item">
+             <IconMenu
+               icon='BIO & CONTACT'
+               iconRipple={false}
+             >
+               <Link className="menu-item" to="/about-me">
+                 <MenuItem className='menu-item' caption='ABOUT ME' />
+               </Link>
+               <Link className="menu-item" to="/health-philosophy">
+                 <MenuItem className='menu-item' caption='HEALTH PHILOSOPHY' />
+               </Link>
+               <Link className="menu-item" to="/testimonials">
+                 <MenuItem className='menu-item' caption='TESTIMONIALS' />
+               </Link>
+               <Link className="menu-item" to="/contact">
+                 <MenuItem className='menu-item' caption='CONTACT' />
+               </Link>
+             </IconMenu>
+           </li>
+         </ul>
+       </div>
+   </nav>
+   )
+  }
  }
+
 
 
 /*   import React, { Component } from 'react';
