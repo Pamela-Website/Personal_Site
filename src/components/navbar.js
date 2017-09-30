fd36faeee27ade3377d26fa7ad017e8577f80527
Ariel <Ariel.Salem1989@gmail.com>
2017-09-30 08:05:23 +0300

    trying different navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,82 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
  import { Link } from 'react-router-dom';
- import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+ import { Collapse, NavbarToggler, Navbar, NavbarBrand, Nav, NavItem, Dropdown, NavLink , DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
  import { Menu, IconMenu, MenuItem } from 'react-toolbox/lib/menu';
 
  export default class Navigation extends Component {
   constructor(props) {
+      super(props);
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render() {
+      return (
+        <div>
+          <Navbar color="faded" light toggleable>
+            <NavbarToggler right onClick={this.toggle} />
+            <NavbarBrand href="/resources"><img className="logo" src="../../public/img/concept4-white.png" /></NavbarBrand>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem className="nav-item">
+                  <NavLink className="nav-link" href="/resources">RESOURCES</NavLink>
+                </NavItem>
+                <NavItem className="nav-item">
+                  <NavLink className="nav-link" href="/recipes">RECIPES</NavLink>
+                </NavItem>
+                <NavItem className="nav-item">
+                  <NavLink className="nav-link" href="/blog">BLOG</NavLink>
+                </NavItem>
+                <NavItem className="nav-item">
+                <IconMenu
+                    icon='SERVICES'
+                    iconRipple={false}
+                  >
+                    <MenuItem>
+                        <Link className="menu-item" to="/nutritional-counseling">NUTRITIONAL COUNSELING</Link>
+                    </MenuItem>
+                    <MenuItem >
+                        <Link className="menu-item" to="/group-sessions">GROUP SESSIONS</Link>
+                    </MenuItem>
+                    <MenuItem>
+                        <Link className="menu-item" to="/corporate-wellness">CORPORATE WELLNESS</Link>
+                    </MenuItem>
+                  </IconMenu>
+                </NavItem>
+                <NavItem className="nav-item">
+                  <IconMenu
+                   icon='BIO & CONTACT'
+                   iconRipple={false}
+                 >
+                   <MenuItem>
+                       <Link className="menu-item" to="/about-me">ABOUT ME</Link>
+                   </MenuItem>
+                   <MenuItem >
+                       <Link className="menu-item" to="/health-philosophy">HEALTH PHILOSOPHY</Link>
+                   </MenuItem>
+                   <MenuItem>
+                       <Link className="menu-item" to="/testimonials">TESTIMONIALS</Link>
+                   </MenuItem>
+                   <MenuItem>
+                       <Link className="menu-item" to="/contact">CONTACT</Link>
+                   </MenuItem>
+                 </IconMenu>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+ }
+/*  constructor(props) {
      super(props);
        this.state = {
          dropdownOpen: false,
@@ -51,14 +123,46 @@
            <li className="nav-item">
              <Link className="nav-link" to="/blog">BLOG</Link>
            </li>
-
+           <li className="nav-item">
+             <IconMenu
+               icon='SERVICES'
+               iconRipple={false}
+             >
+               <Link className='menu-item' to='/nutritional-counseling' >
+                 <MenuItem className='menu-item' caption='NUTRITIONAL COUNSELING' />
+               </Link>
+               <Link className="menu-item" to="/group-sessions">
+                 <MenuItem className='menu-item' caption='GROUP SESSIONS' />
+               </Link>
+               <Link className="menu-item" to="/corporate-wellness">
+                 <MenuItem className='menu-item' caption='CORPORATE WELLNESS' />
+               </Link>
+             </IconMenu>
+           </li>
+           <li className="nav-item">
+             <IconMenu
+               icon='BIO & CONTACT'
+               iconRipple={false}
+             >
+               <Link className="menu-item" to="/about-me">
+                 <MenuItem className='menu-item' caption='ABOUT ME' />
+               </Link>
+               <Link className="menu-item" to="/health-philosophy">
+                 <MenuItem className='menu-item' caption='HEALTH PHILOSOPHY' />
+               </Link>
+               <Link className="menu-item" to="/testimonials">
+                 <MenuItem className='menu-item' caption='TESTIMONIALS' />
+               </Link>
+               <Link className="menu-item" to="/contact">
+                 <MenuItem className='menu-item' caption='CONTACT' />
+               </Link>
+             </IconMenu>
+           </li>
          </ul>
        </div>
    </nav>
    )
-  }
- }
-
+  }*/
 
 
 /*   import React, { Component } from 'react';
