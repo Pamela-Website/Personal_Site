5e76b3fa2e2025fcfaf1f55ef1acffea4a56bc8c
Ariel <Ariel.Salem1989@gmail.com>
2017-10-21 09:15:57 -0700

    pushed safari styling changes to heroku

--- public/oauth.js
+++
@@ -1,6 +0,0 @@
-module.exports.user = 'nourishroots1@gmail.com';
-module.exports.domain = 'nourishroots.com';
-module.exports.api_key = 'key-f01f1bbaf27d02f3d18898adcda507aa';
-module.exports.drive_api = 'AIzaSyBzw5GPU4aZpXsVcp2JL9i-NRYH4epnRk8';
-module.exports.client_id = '48107214957-lrf9lfoq3ge8h1nasv5re82pfe9fo2m1.apps.googleusercontent.com';
-module.exports.secret = 'NsQVaZAnQkVQUqc9dSOvkQoF';
